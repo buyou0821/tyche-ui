bba2e1d6f9adad0a9c5f9dcf0ee11f2e62750778
shenming <buyou0821@gmail.com>
2019-03-27 09:58:59 +0800

    :wrench: tsconfig

--- components/icon/index.tsx
+++ components/icon/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
-// interface IconProps {
-//   name: string;
-// }
-
-function Icon() {
-  return <div>Icon</div>;
+interface IconProps {
+  name?: string;
 }
+
+interface IconComponent<p> extends React.FunctionComponent<p> {}
+
+const Icon: IconComponent<IconProps> = props => {
+  return <div>{props.name}</div>;
+};
 
 export default Icon;
--- components/index.tsx
+++ components/index.tsx
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Icon from './icon';
 
-ReactDOM.render(<Icon />, document.getElementById('root'));
+ReactDOM.render(<Icon name="wechat" />, document.getElementById('root'));
