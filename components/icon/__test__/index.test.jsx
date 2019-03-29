0baca234f60b04784d7dca035e3f7e606f29e86c
shenming <buyou0821@gmail.com>
2019-03-29 15:16:38 +0800

    :white_check_mark: test Icon component

---
+++ components/icon/__test__/index.test.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Icon from '..';
+import { render, mount } from 'enzyme';
+
+describe('Icon', () => {
+  it('should render material icon', () => {
+    const wrapper = render(<Icon material="face" />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should support iconfont', () => {
+    const IconFont = Icon.createFromIconfont({
+      scriptUrl: '//at.alicdn.com/t/font_8d5l8fzk5b87iudi.js',
+    });
+    const wrapper = render(<IconFont type="icon-twitter" />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should support onClick', () => {
+    const fn = jest.fn();
+    const wrapper = mount(<Icon material="face" onClick={fn} />);
+    wrapper.simulate('click');
+    expect(fn).toBeCalled();
+  });
+});
--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,28 @@
 module.exports = {
   verbose: true,
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'md'],
-  // setupFiles: ['./tests/setup.js'],
+  setupFiles: ['./tests/setup.js'],
   globals: {
     'ts-jest': {
       tsConfig: './tsconfig.test.json',
     },
   },
+  transform: {
+    '^.+test\\.(js|jsx)$': 'babel-jest',
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/tests/__mocks__/file-mock.js',
+    '\\.(css|sass|scss)$': '<rootDir>/tests/__mocks__/object-mock.js',
+  },
+  collectCoverageFrom: [
+    'components/**/*.{ts,tsx}',
+    '!components/*/style/index.tsx',
+    '!components/style/index.tsx',
+    '!components/*/locale/index.tsx',
+    '!components/*/__tests__/**/type.tsx',
+    '!components/**/*/interface.{ts,tsx}',
+  ],
+  snapshotSerializers: ['enzyme-to-json/serializer'],
 };
