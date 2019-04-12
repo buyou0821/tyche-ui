75e7bce89a3b8200eb2c2857ba12e781fad24f48
shenming <buyou0821@gmail.com>
2019-04-12 14:00:48 +0800

    :sparkles: ripple transition

--- components/ripple/Ripple.tsx
+++ components/ripple/Ripple.tsx
@@ -28,12 +28,6 @@ const Ripple = (props: RippleProps) => {
     [`childLeaving`]: leaving,
   });
 
-  // const rippleClassName = clsx({
-  //   // [`ripple`]: true,
-  //   [`rippleVisible`]: visible,
-  //   [`childLeaving`]: leaving,
-  // });
-
   return (
     <Transition
       {...restProps}
@@ -55,74 +49,8 @@ const Ripple = (props: RippleProps) => {
       >
         <div className={childClassName} />
       </div>
-      {/* <div
-        className={rippleClassName}
-        style={{
-          width: rippleSize,
-          height: rippleSize,
-          top: rippleY - rippleSize / 2,
-          left: rippleX - rippleSize / 2,
-          backgroundColor: '#000',
-        }}
-      /> */}
     </Transition>
   );
 };
-
-// const Ripple = (props: RippleProps) => {
-//   const { rippleX, rippleY, rippleSize } = props;
-//   return (
-//     <CSSTransition
-//       onEnter={() => {
-//         console.log(1);
-//       }}
-//       timeout={500}
-//     >
-//       <div
-//         className="ripple"
-//         style={{
-//           width: rippleSize,
-//           height: rippleSize,
-//           top: rippleY - rippleSize / 2,
-//           left: rippleX - rippleSize / 2,
-//           backgroundColor: '#000',
-//         }}
-//       >
-//         123
-//       </div>
-//     </CSSTransition>
-//   );
-// };
-
-// class Ripple extends React.Component<RippleProps, {}> {
-//   handleEnter = () => {
-//     console.log('handleEnter');
-//   };
-//   render() {
-//     return (
-//       <Transition in={this.props.in} onEnter={this.handleEnter} timeout={500}>
-//         <span>123</span>
-//       </Transition>
-//     );
-//   }
-// }
-
-// const Ripple = (props: RippleProps) => {
-//   const { rippleX, rippleY, rippleSize } = props;
-//   return (
-//     <div
-//       className={'ripple ' + props.classNames}
-//       style={{
-//         width: rippleSize,
-//         height: rippleSize,
-//         top: rippleY - rippleSize / 2,
-//         left: rippleX - rippleSize / 2,
-//         backgroundColor: '#000',
-//       }}
-//     >
-//       123
-//     </div>
-//   );
-// };
 
 export default Ripple;
